d9e96faccb958036ee5c8224c1608b44ff229817
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-01 01:07:29 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function introduction(name){
 function introductionWithLanguage(name, language){
   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-function introductionWithLanguageOptional(name, language="Javascript"){
+function introductionWithLanguageOptional(name, language="JavaScript"){
   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-function introductionWithLanguageOptional(name, language="Javascript"){
-  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-}
+// function introductionWithLanguageOptional(name, language="Javascript"){
+//   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+// }
